32fe5ce949d1bc307a5008905496651bff3b427f
Марина Астахова <Charlie@MacBook-Pro-Marina.local>
2020-08-29 21:24:17 +0300

    111

---
+++ wolf3d_ahusk/includes/wolf3d.h
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wolf3d.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahusk <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/13 20:15:20 by ahusk             #+#    #+#             */
+/*   Updated: 2020/08/13 20:15:24 by ahusk            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WOLF3D_H
+# define WOLF3D_H
+
+# include "SDL.h"
+# include <math.h>
+# include <pthread.h>
+# include <stdbool.h>
+# include <unistd.h>
+//# include <string.h>
+//# include <stdlib.h>
+# include <fcntl.h>
+//# include <stdio.h>
+# define PI 3.14
+# define W_W 1064
+# define W_H 512
+# define RGB(r, g, b) r + (g << 8) + (b << 16)
+# define FOV PI/3
+
+typedef struct		s_raycast
+{
+	float			x;
+	float			y;
+}					t_raycast;
+
+typedef struct		s_rect
+{
+	int				x;
+	int				y;
+	int				h;
+	int				color;
+}					t_rect;
+
+typedef struct		s_player
+{
+	float			angle;
+	float			x;
+	float			y;
+	int				speed;
+	int				mouse_x;
+	t_rect			rect;
+}					t_player;
+
+typedef struct		s_map
+{
+	int				h;
+	int				w;
+	int				rect_w;
+	int				*map;
+	t_rect			rect;
+}					t_map;
+
+typedef struct		s_sdl
+{
+	SDL_Surface		*scrs;
+	SDL_Surface		*textures;
+	SDL_Renderer	*rend;
+	SDL_Window		*win;
+	SDL_Texture		*win_texture;
+	SDL_Event		e;
+	int				y;
+	unsigned char	*bytes;
+	unsigned char	*bytes_texture;
+	int				pitch;
+	bool			run;
+}					t_sdl;
+
+typedef struct		s_main
+{
+	t_player		player;
+	t_map			map;
+	t_sdl			sdl;
+	t_raycast		raycast;
+	t_rect			rect;
+}					t_main;
+
+void				draw_map(t_map *map, t_sdl *sdl);
+void				draw_fov(t_sdl *sdl, t_player *player);
+void				draw_rect(t_sdl *sdl, t_rect *rect);
+void				draw_walls(t_main *main, t_player *player, t_raycast *raycast, t_map *map);
+void				input_pixel(t_sdl *sdl, int x, int y, int color);
+void				draw_texture(t_sdl *sdl, int x, int y, int xt);
+void				draw_skybox(t_sdl *sdl, t_player *player);
+void				init(t_main *main);
+void				error(int i);
+void				read_map(t_main *main, char *str);
+
+#endif
---
+++ wolf3d_ahusk/src/main.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahusk <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/22 00:19:54 by ahusk             #+#    #+#             */
+/*   Updated: 2020/08/22 00:19:56 by ahusk            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "wolf3d.h"
+
+void	quit(t_sdl *sdl)
+{
+	SDL_DestroyRenderer(sdl->rend);
+	sdl->rend = NULL;
+	SDL_DestroyTexture(sdl->win_texture);
+	sdl->win_texture = NULL;
+	SDL_DestroyWindow(sdl->win);
+	sdl->win = NULL;
+	SDL_Quit();
+}
+
+void	hooks(t_sdl *sdl, t_map *map, t_player *player)
+{
+	float		tmp_x;
+	float		tmp_y;
+
+	while (SDL_PollEvent(&sdl->e) != 0)
+	{
+		if (sdl->e.type == SDL_QUIT)
+			sdl->run = false;
+		if (sdl->e.type == SDL_MOUSEMOTION)
+		{
+			if (sdl->e.motion.x - player->mouse_x > 0 || sdl->e.motion.x == W_W - 1)
+				player->angle += 0.05;
+			else
+				player->angle -= 0.05;
+			player->mouse_x = sdl->e.motion.x;
+		}
+		if (sdl->e.type == SDL_KEYDOWN)
+		{
+			if (sdl->e.key.keysym.sym == SDLK_ESCAPE)
+				sdl->run = false;
+			tmp_x = cos(player->angle) * player->speed;
+			tmp_y = sin(player->angle) * player->speed;
+			if (sdl->e.key.keysym.sym == SDLK_UP || sdl->e.key.keysym.sym == SDLK_w)
+			{
+				if ((map->map[
+						((int)(player->x + tmp_x) / map->rect_w) + (int)(player->y + tmp_y) / map->rect_w * map->w
+							]) == 0)
+				{
+					player->x += tmp_x;
+					player->y += tmp_y;
+				}
+			}
+			if (sdl->e.key.keysym.sym == SDLK_DOWN || sdl->e.key.keysym.sym == SDLK_s)
+			{
+				if ((map->map[
+						((int)(player->x - tmp_x) / map->rect_w) + (int)(player->y - tmp_y) / map->rect_w * map->w
+							]) == 0)
+				{
+					player->x -= tmp_x;
+					player->y -= tmp_y;
+				}
+			}
+			if (sdl->e.key.keysym.sym == SDLK_RIGHT || sdl->e.key.keysym.sym == SDLK_d)
+			{
+				if ((map->map[
+						((int)(player->x - tmp_y) / map->rect_w) + (int)(player->y + tmp_x) / map->rect_w * map->w
+							]) == 0)
+				{
+					player->x -= tmp_y;
+					player->y += tmp_x;
+				}
+			}
+			if (sdl->e.key.keysym.sym == SDLK_LEFT || sdl->e.key.keysym.sym == SDLK_a)
+			{
+				if ((map->map[
+						((int)(player->x + tmp_y) / map->rect_w) + (int)(player->y - tmp_x) / map->rect_w * map->w
+							]) == 0)
+				{
+					player->x += tmp_y;
+					player->y -= tmp_x;
+				}
+			}
+		}
+	}
+}
+
+int		main(int arg, char **argv)
+{
+	t_main main;
+
+	if (arg != 2)
+		error(1);
+	init(&main);
+	read_map(&main, argv[1]);
+	while (main.sdl.run)
+	{
+		hooks(&main.sdl, &main.map, &main.player);
+		SDL_LockTexture(main.sdl.win_texture, NULL, &main.sdl.bytes, &main.sdl.pitch);
+		draw_skybox(&main.sdl, &main.player);
+		draw_walls(&main, &main, &main.raycast, &main.map);
+		draw_map(&main.map, &main.sdl);
+		draw_fov(&main.sdl, &main.player);
+		main.player.rect.x = main.player.x - 3;
+		main.player.rect.y = main.player.y - 3;
+		draw_rect(&main.sdl, &main.player.rect);
+		SDL_UnlockTexture(main.sdl.win_texture);
+		SDL_RenderCopy(main.sdl.rend, main.sdl.win_texture, NULL, NULL);
+		SDL_RenderPresent(main.sdl.rend);
+	}
+	quit(&main.sdl);
+	return (0);
+}
